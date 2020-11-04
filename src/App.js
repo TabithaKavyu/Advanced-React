e0a8461cbe9e32f20747efbfddd157dae4dc13c6
tabit <tabithakavyu4@gmail.com>
2020-11-04 09:41:19 +0300

    useStateArray example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import Setup from './tutorial/1-useState/setup/2-useState-basics'
+import Setup from './tutorial/1-useState/setup/3-useState-array'
+
 
 function App() {
   return (
