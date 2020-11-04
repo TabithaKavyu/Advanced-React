3b88aac7e6f59249a0077cb040e5c23581eb8250
tabit <tabithakavyu4@gmail.com>
2020-11-04 11:59:55 +0300

    Functional update form

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -8,6 +8,15 @@ const UseStateCounter = () => {
     setValue(0);
   };
 
+  const complexIncrease = () => {
+    setTimeout(() => {
+      //setValue(value + 1);
+      setValue((prevState) => {
+        return prevState + 1;
+      })
+    }, 2000);
+  };
+
   return (
     <>
       <section style={{ margin: '4rem 0' }}>
@@ -17,6 +26,11 @@ const UseStateCounter = () => {
         <button className='btn' onClick={reset}>reset</button>
         <button className='btn' onClick={() => setValue(value + 1)}>Increase</button>
       </section>
+      <section style={{ margin: '4rem 0' }}>
+        <h2>more complex counter</h2>
+        <h1>{value}</h1>
+        <button className='btn' onClick={complexIncrease}>increase later</button>
+        </section>
     </>
   );
 };
