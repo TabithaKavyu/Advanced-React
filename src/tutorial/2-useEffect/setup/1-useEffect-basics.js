a2d7a20b3613631186aa077970d0a7614c3e8dd6
tabit <tabithakavyu4@gmail.com>
2020-11-12 16:44:06 +0300

    useEffect basics

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,21 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    console.log('call useEffect');
+    document.title = `New Messages(${value})`;
+  });
+  console.log('render componenet');
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className='btn' onClick={() => setValue(value + 1)}>Click Me</button>
+  
+  
+    </>
+  );
 };
 
 export default UseEffectBasics;
